a94fdcc5427d4c5dfaf59ed1043e3ec8a01ae0ea
Henry Gann <henry.gann@gmail.com>
2023-01-04 14:47:39 +1300

    Basic put implementation added.

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -1,5 +1,6 @@
 import express from "express"
-import { doc, getDoc, updateDoc } from "firebase/firestore"
+import crypto from "crypto"
+import { doc, getDoc, updateDoc, arrayUnion } from "firebase/firestore"
 import { db } from "../../Firebase/FirebaseConfig.js"
 
 /*
@@ -11,16 +12,37 @@ const router = express.Router()
 router.get("/", async (req, res) => {
   const docRef = doc(db, "user-items", req.query.id)
   const docSnap = await getDoc(docRef)
-  //console.log(docSnap.data())
   const rawData = docSnap.data()
-  let formattedData = []
-  Object.keys(rawData).forEach((key, index) => {
-    formattedData.push(rawData[key])
-  })
-  res.status(200).json(formattedData)
-  //res.send("yeetem")
+  res.status(200).json(rawData.items)
 })
 
-router.post("/", async (req, res) => {})
+router.post("/", async (req, res) => {
+  const newItem = {
+    _id: crypto.randomUUID(),
+    name: "new",
+    x: 5,
+    y: 6,
+    z: 7,
+    mass: 5,
+  }
+  const docRef = doc(db, "user-items", req.query.id)
+  const newDoc = await updateDoc(docRef, { items: arrayUnion(newItem) })
+  console.log(newDoc)
+  res.status(200).json("Success")
+})
+
+router.put("/", async (req, res) => {
+  const docRef = doc(db, "user-items", req.query.id)
+  const docSnap = await getDoc(docRef)
+  const items = docSnap.data().items
+  console.log(items)
+  const index = items.findIndex(
+    (x) => x._id === "3243984a-d065-4a6e-aeaa-0628ba733a6d"
+  )
+  items[index].name = "putName"
+  console.log(items)
+  await updateDoc(docRef, { items: items })
+  res.status(200).json(crypto.randomUUID())
+})
 
 export default router
