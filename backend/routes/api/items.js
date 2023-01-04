80006040456fd53c3a2447541a4f12310c3f30c6
Henry Gann <henry.gann@gmail.com>
2023-01-04 15:33:58 +1300

    Added delete functionality.

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -1,48 +1,69 @@
 import express from "express"
 import crypto from "crypto"
-import { doc, getDoc, updateDoc, arrayUnion } from "firebase/firestore"
+import {
+  doc,
+  getDoc,
+  updateDoc,
+  arrayUnion,
+  arrayRemove,
+} from "firebase/firestore"
 import { db } from "../../Firebase/FirebaseConfig.js"
 
 /*
-    Items router contains all actions under /api/items.
+    The items router contains all actions under /api/items.
     Takes query parameter ID to determine the user wanting their items
 */
 const router = express.Router()
 
 router.get("/", async (req, res) => {
-  const docRef = doc(db, "user-items", req.query.id)
-  const docSnap = await getDoc(docRef)
-  const rawData = docSnap.data()
-  res.status(200).json(rawData.items)
+  const items = await getItems(req.query.id)
+  res.status(200).json(items)
 })
 
 router.post("/", async (req, res) => {
+  const docRef = doc(db, "user-items", req.query.id)
   const newItem = {
     _id: crypto.randomUUID(),
-    name: "new",
-    x: 5,
-    y: 6,
-    z: 7,
-    mass: 5,
+    name: "tester",
+    x: 2,
+    y: 4,
+    z: 5,
+    mass: 10,
   }
-  const docRef = doc(db, "user-items", req.query.id)
-  const newDoc = await updateDoc(docRef, { items: arrayUnion(newItem) })
-  console.log(newDoc)
+  await updateDoc(docRef, { items: arrayUnion(newItem) })
   res.status(200).json("Success")
 })
 
 router.put("/", async (req, res) => {
   const docRef = doc(db, "user-items", req.query.id)
-  const docSnap = await getDoc(docRef)
-  const items = docSnap.data().items
-  console.log(items)
+  const items = await getItems(req.query.id)
   const index = items.findIndex(
     (x) => x._id === "3243984a-d065-4a6e-aeaa-0628ba733a6d"
   )
-  items[index].name = "putName"
-  console.log(items)
+  items[index].name = "putTest"
   await updateDoc(docRef, { items: items })
   res.status(200).json(crypto.randomUUID())
 })
+
+// If an item is supplied in the DELETE, remove that item, otherwise purge the document
+router.delete("/", async (req, res) => {
+  const docRef = doc(db, "user-items", req.query.id)
+  console.log(req.body.item)
+  console.log(typeof req.body.item)
+  if (req.body.item) {
+    console.log("removing")
+    await updateDoc(docRef, { items: arrayRemove(req.body.item) })
+    res.send(200)
+  } else {
+    await updateDoc(docRef, { items: [] })
+    res.send(200)
+  }
+})
+
+const getItems = async (id) => {
+  const docRef = doc(db, "user-items", id)
+  const docSnap = await getDoc(docRef)
+  return docSnap.data().items
+}
 
 export default router
